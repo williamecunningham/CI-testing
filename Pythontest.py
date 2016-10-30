bf706b6fb9105afe9c22277f9d21e0fdd08d3787
williamecunningham <williamecunningham@gmail.com>
2016-10-30 10:08:39 -0600

    Initial file for ci testing
    An initial file to use to try to bring up jenkins testing from a
    container

---
+++ Pythontest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def thisIsaTest():
+	print "Hello World!"
+	print "Hello Again"
+	print "I like typing this."
+	print "This is fun."
+	print 'Yay! Printing.'
+	print "I'd much rather you 'not'."
+	print 'I "said" do not touch this.'
+
+thisIsaTest()
